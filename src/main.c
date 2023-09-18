7ef077845371520e436eb315e2186d94bf76789a
trodion <rodion.toporoff@yandex.ru>
2023-09-18 20:05:24 +0300

    Timer 1 to 10

--- src/main.c
+++ src/main.c
@@ -2,9 +2,12 @@
 #include "lcd_driver.h"
 #include "font.h"
 
+#define START (TIM6->CR1 |= TIM_CR1_CEN)
+
 void pin_init(); /* Настройка выводов */
 void init_rcc(); /* Настройка системы тактирования */
 void init_NVIC(); /* Настройка контроллера прерываний */
+void init_TIM6(); /* Настройка таймера */
 
 void EXTI0_IRQHandler(void) {
 	/* Обработчик прерывания от линии EXTI0, 
@@ -17,6 +20,25 @@ void EXTI0_IRQHandler(void) {
 	ms_delay(1000);
 }
 
+void TIM6_DAC_IRQHandler(void){
+    static uint8_t* ptr = &num; /* Изображение хранится ввиде байтов в массиве, указатель на массив */
+	static uint8_t count = 5; /*  */
+	if (count == 50){
+		ptr = &num;
+		count = 5;
+	} 
+    
+	set_page(1);
+	set_col(0x3, 0xE);
+	
+	draw_image(ptr, 5);
+	ptr += 5;
+	count += 5;
+	
+	// Сбросить событие обновления
+    TIM6->SR &= ~TIM_SR_UIF;
+}
+
 int main() {
 	init_rcc();
 	pin_init();
@@ -24,9 +46,10 @@ int main() {
 	lcd_init();
 	init_NVIC();
 
-	set_page(1);
-	set_col(0, 0x5);
-	draw_image(*num, 45);
+	init_TIM6();
+	
+	ms_delay(2000);
+	START;
 
 	while(1){};
 	return 0;
@@ -34,7 +57,8 @@ int main() {
 
 void init_rcc() {
 	/* Включение тактирования порта А, интерфейса SPI1 */
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; 
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN; 
+	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
 }
 
 void pin_init() {
@@ -58,8 +82,8 @@ void pin_init() {
 	GPIOA->CRL |= GPIO_CRL_MODE7_1 | GPIO_CRL_MODE7_0;
 	GPIOA->CRL &= ~GPIO_CRL_CNF7_0 & ~GPIO_CRL_CNF7_1;
 
-	GPIOA->CRL |= GPIO_CRL_CNF0_0;
-	GPIOA->CRL &= ~GPIO_CRL_MODE0_1 & ~GPIO_CRL_MODE0_0 & ~GPIO_CRL_CNF0_1;
+	GPIOC->CRH &= ~GPIO_CRH_CNF8_0 & ~GPIO_CRH_CNF8_1 ;
+    GPIOC->CRH |= GPIO_CRH_MODE8_0 | GPIO_CRH_MODE8_1;
 }
 
 void init_NVIC() {
@@ -70,4 +94,14 @@ void init_NVIC() {
     
     NVIC_SetPriority(EXTI0_IRQn, 0);
     NVIC_EnableIRQ(EXTI0_IRQn);
+}
+
+void init_TIM6() {
+	// Предделитель
+    TIM6->PSC = 8000 - 1;
+    // Максимальное значение ва CNT
+    TIM6->ARR = 1000;
+    // Разрешение генерации прерывания по событию сброса счетчика
+    TIM6->DIER |= TIM_DIER_UIE;
+    NVIC_EnableIRQ(TIM6_DAC_IRQn);  
 }
